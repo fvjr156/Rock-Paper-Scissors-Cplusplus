093bc7a614b949f556cdcacd8bd49d0dace1a36a
VanzTyl <125661587+VanzTyl@users.noreply.github.com>
2023-09-24 01:52:04 +0800

    Add files via upload

---
+++ Classes.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_LMS_Activity_1
+{
+    public class Player
+    {
+        public string name { get; set; }
+        public int damage { get; set; }
+        public int hp { get; set; }
+        public int wins { get; set; }
+        public int winstreak { get; set; }
+        public int losses { get; set; }
+        public string choice { get; set; }
+    }
+    public class Bot
+    {
+        public string name { get; set; }
+        public int damage { get; set; }
+        public int hp { get; set; }
+        public string choice { get; set; }
+    }
+}
